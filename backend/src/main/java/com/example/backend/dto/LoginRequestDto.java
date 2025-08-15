fd9c6b32d3154c3f5a7460e862635176821a05bc
macklinkim <kopserf@gmali.com>
2025-08-15 18:13:52 +0900

    update DTO and controller for swagger.

--- backend/src/main/java/com/example/backend/dto/LoginRequestDto.java
+++ backend/src/main/java/com/example/backend/dto/LoginRequestDto.java
@@ -3,7 +3,7 @@ package com.example.backend.dto;
 import lombok.Data;
 
 @Data
-public class LoginRequest {
+public class LoginRequestDto {
     private String username;
     private String password;
 }
